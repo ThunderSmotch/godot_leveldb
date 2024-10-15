79d4aea506321e25d0c79fe8b641c657913b1a3a
Carlos Couto <2165764+ThunderSmotch@users.noreply.github.com>
2024-10-15 10:52:11 +0100

    docs: Update demo project and docs

--- demo/main.gd
+++ demo/main.gd
@@ -2,16 +2,37 @@ extends Node
 
 var db = GDLevelDB.new()
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Test LevelDB")
-	db.open("./leveldb")
+	# Opening database (creating one if it does not exist)
+	db.open("./demo_db", {"create_if_missing": true})
+	
+	# Saving a string and an integer
+	var a_key: PackedByteArray = "key1".to_ascii_buffer()
+	var other_key: PackedByteArray = "key2".to_ascii_buffer()
+	
+	var string: String = "value"
+	
+	var i: int = 1000
+	var bytes: PackedByteArray
+	bytes.resize(8) # 8 bytes = 64 bits, enough for an int
+	bytes.encode_s64(0, i)
+	
+	db.put(a_key, string.to_ascii_buffer())
+	db.put(other_key, bytes)
+	
+	# Print all the keys in this database assuming they were strings
 	var keys = db.keys()
-	#print(db.get(keys[3]).get_string_from_ascii())
-	var key: PackedByteArray = keys[3]
-	print(key.get_string_from_utf8())
-
+	for key in keys:
+		db.print(key)
 	
-	db.print(keys[3])
+	# Print the string
+	db.print(db.get(a_key))
 	
+	# Grab signed 64-bit int value
+	print(db.get(other_key).decode_s64(0))
 	
+	# Deleting a key/value pair
+	db.delete(a_key)
+	
+	# Close the database
+	db.close()
